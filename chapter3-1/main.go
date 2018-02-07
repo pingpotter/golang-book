af341637d75c932108247f3d807219a2aa39a7b0
jiraphonSa <jiraphon.sa@tnis.com>
2018-02-07 10:19:24 +0700

    Add chapter 3-1 zero value

---
+++ chapter3-1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	fmt.Println("===============Zero Value====================")
+	var number int
+	var str string
+	var boolean bool
+	fmt.Printf("number: %v : '%v'\n", number,str)
+	fmt.Printf("str: '%v'\n", str)
+	fmt.Printf("line: %v\n", boolean)
+}
