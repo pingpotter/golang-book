e9ae19fbb51b6131b0fcbe7a42b2f320fc90999f
jiraphonSa <jiraphon.sa@tnis.com>
2018-02-08 09:45:59 +0700

    Add new folder 4-1

--- chapter3-3/main.go
+++ chapter3-3/main.go
@@ -11,8 +11,10 @@ func main() {
 	fmt.Println("==============Comparing floating point============")
 
 	fmt.Println("0.1 + 0.2 == 0.3 is ", 0.1+0.2 == 0.3)
-	num := 0.1
+	var num float32
+	num = 0.1
 	num += 0.2
 	fmt.Println("num == 0.3 is ", num == 0.3)
 	fmt.Println("num is ", num)
+	//https://stackoverflow.com/questions/22337418/golang-floating-point-precision-float32-vs-float64
 }
