0139f8980e7290eeef3f7946d91c8201e0410c9a
pingpotter <pingpotter@live.com>
2018-03-09 11:35:34 +0700

    Edit exercise 4

--- exercise/speed-espresso.go
+++ exercise/speed-espresso.go
@@ -10,19 +10,21 @@ func main() {
 	volumn := 200
 	start := time.Now()
 
-	container := order(volumn)
+	order(volumn)
+	/*container := order(volumn)
 	for _, cup := range container {
 		fmt.Println(cup)
 	}
-
+	*/
 	end := time.Now()
 
 	start2 := time.Now()
-	container2 := order2(volumn)
+	order2(volumn)
+	/*container2 := order2(volumn)
 	for _, cup := range container2 {
 		fmt.Println(cup)
 	}
-
+	*/
 	end2 := time.Now()
 
 	fmt.Println("Cafe 1", end.Sub(start))
@@ -35,20 +37,10 @@ func order2(volumn int) (container []string) {
 	order := make(chan string)
 	bar := make(chan string)
 
-	go func() {
-		for i := 1; i <= volumn; i++ {
-			order <- reCive(i)
-		}
-		close(order)
-	}()
-
-	go func() {
-		for coffee := range order {
-			bar <- brew(coffee)
-		}
-
-		close(bar)
-	}()
+	go reCive2(order, volumn)
+	go brew2(order, bar)
+	go brew2(order, bar)
+	go brew2(order, bar)
 
 	for x := range bar {
 		container = append(container, serve(x))
@@ -79,6 +71,24 @@ func order(volumn int) (container []string) {
 	}
 
 	return
+}
+
+func reCive2(out chan<- string, volumn int) {
+	for i := 0; i <= volumn; i++ {
+
+		time.Sleep(5 * time.Millisecond)
+		out <- fmt.Sprintf("order: %d", i)
+	}
+	close(out)
+}
+
+func brew2(in <-chan string, out chan<- string) {
+
+	for x := range in {
+		time.Sleep(100 * time.Millisecond)
+		out <- fmt.Sprintf("%s %s", x, "espresso")
+	}
+	close(out)
 }
 
 func reCive(number int) string {
